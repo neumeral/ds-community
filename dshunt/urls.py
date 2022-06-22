47723a4c9c940fff86320ce1642766beebbe001b
Bibin <bibinbabyanthony@gmail.com>
2022-06-23 00:48:09 +0530

    Post creation for book

--- dshunt/urls.py
+++ dshunt/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 from . import views
 
@@ -26,9 +25,13 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
 
-
     path('', views.post_list, name="post-list"),
-    path('post/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/', views.PostSubmitPageView.as_view(), name='post-submit'),
+
+    path('books/new/', views.BookCreateView.as_view(), name='book-create'),
+    # path('videos/new/', views.video_create, name='video-create'),
+    # path('books/new/', views.BookCreateView.as_view(), name='tutorial-create'),
+    # path('books/new/', views.BookCreateView.as_view(), name='podcast-episode--create'),
 
     # category
     path('category/', views.category, name='category'),
--- dshunt/views.py
+++ dshunt/views.py
@@ -1,57 +1,87 @@
 import datetime
-from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic import CreateView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
 
-from .forms import CreatePostForm
+from .forms import BookCreateForm, VideoCreateForm, TutorialCreateForm, PodcastEpisodeCreateForm, PostTypeForm
 from .models import AppUser, Post, Category, PostVote
+
 
 def post_list(request):
     di = {}
     d = datetime.date.today()
-    postvote = PostVote.objects.all()
+    post_vote = PostVote.objects.all()
     post = Post.objects.all()
 
     for i in range(7):
         idate = d-datetime.timedelta(days=i)
-        post = PostVote.objects.filter(post__date_published = idate, post__approved=True).order_by('-vote')[:5]
+        posts = PostVote.objects.filter(post__published_at=idate, post__approved=True)[:5]
+        post = sorted(posts, key=lambda obj: obj.vote_count, reverse=True)
         di[idate] = [(x.post, x.vote) for x in post]
-    
+
     # di[d-datetime.timedelta(days=2)] = Post.objects.filter(date_published=d-datetime.timedelta(days=2))
     # di[d-datetime.timedelta(days=3)] = Post.objects.filter(date_published=d-datetime.timedelta(days=3))
     # di[d-datetime.timedelta(days=4)] = Post.objects.filter(date_published=d-datetime.timedelta(days=4))
     # di[d-datetime.timedelta(days=5)] = Post.objects.filter(date_published=d-datetime.timedelta(days=5))
     # di[d-datetime.timedelta(days=6)] = Post.objects.filter(date_published=d-datetime.timedelta(days=6))
-    
-    context = {'post':di, 'd':d-datetime.timedelta(days=1)}
+
+    context = {'post': di, 'd': d-datetime.timedelta(days=1)}
     return render(request, 'posts.html', context)
 
 
-class PostCreateView(View):
-    form_class = CreatePostForm
+def post_submit_page(request):
+    return render(request, 'dshunt/post_submit.html')
+
+
+class PostSubmitPageView(View):
     template_name = 'dshunt/post_submit.html'
+    form = PostTypeForm
 
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'form': self.form})
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if form.is_valid():
+            post_type = form.cleaned_data.get('post_type')
+            post_views = {'Book': 'book-create', 'Video': 'video-create', 'Tutorial': 'tutorial-create',
+                          'Podcast': 'podcast-episode-create'}
+            return redirect(post_views.get(post_type))
+        self.get(request)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['form'] = PostTypeForm()
+    #     context['book_form'] = BookCreateForm()
+    #     context['video_form'] = VideoCreateForm()
+    #     context['tutorial_form'] = TutorialCreateForm()
+    #     context['podcast_form'] = PodcastEpisodeCreateForm()
+    #     return context
+
+
+class BookCreateView(View):
+    form_class = BookCreateForm
+    template_name = 'dshunt/book_create_form.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'book_form': self.form_class()})
+
+    def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            form = form.save(commit=False)
-            form.submitted_user = request.user
-            form.save()
-
-            # save to PostVote
-            p = PostVote(post = form)
-            p.save()
+            book = form.save(commit=False)
+            book.created_user = self.request.user
+            book.post_type = 'Book'
+            book.save()
+            return reverse_lazy('post-list')
         else:
-            return render(request, self.template_name, {'form': form})
-        return redirect(postList)
+            return render(request, self.template_name, {'book_form': form})
 
 
 # Voting to Post
+
 class Vote(View):
 
     def post(self, request, id):
@@ -66,9 +96,11 @@ class Vote(View):
                 post=post, vote=1
             )
             postvote.save()
-        return redirect(postList)
+        return redirect('home')
+
 
 # CATEGORY
+
 def category(request):
     cat = Category.objects.all()
     context = {'category':cat}
